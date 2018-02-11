d957cfbd7f7bf5fcec23c77b9d145f7982705b5d
Mohd. Asfaq-E-Azam Rifat <Rifat.kps@gmail.com>
2018-02-11 16:28:19 +0600

    Add DataBinding library

--- app/src/main/java/android/development/com/helloosmdroid/MainActivity.java
+++ app/src/main/java/android/development/com/helloosmdroid/MainActivity.java
@@ -1,13 +1,18 @@
 package android.development.com.helloosmdroid;
 
+import android.databinding.DataBindingUtil;
+import android.development.com.helloosmdroid.databinding.ActivityMainBinding;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
 public class MainActivity extends AppCompatActivity {
 
+    // Private fields
+    ActivityMainBinding mBinding;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        mBinding = DataBindingUtil.setContentView(this, R.layout.activity_main);
     }
 }
